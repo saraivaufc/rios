484b6473121d4b31b3040c81a1c4d01af5dc14c3
Marciano Saraiva <saraiva.ufc@gmail.com>
2019-11-16 20:26:54 -0300

    update to 1.4.8 version

---
+++ build/scripts-3.6/rioscalcstats.py
@@ -0,0 +1,9 @@
+#!/mnt/data/Documents/mapreduce/env/bin/python3
+"""
+Use rios.calcstats to calculate statistics for the given image(s). 
+
+"""
+
+from rios.cmdline import rioscalcstats
+
+rioscalcstats.main()
---
+++ build/scripts-3.6/riosprintstats.py
@@ -0,0 +1,9 @@
+#!/mnt/data/Documents/mapreduce/env/bin/python3
+"""
+Use rios.fileinfo to print the statistics for the given image(s). 
+
+"""
+
+from rios.cmdline import riosprintstats
+
+riosprintstats.main()
